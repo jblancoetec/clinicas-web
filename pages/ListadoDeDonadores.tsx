77767c3bc8be601f225376bb02e01c2adf07d2cc
Zack <mmamaniticona@etec.uba.ar>
2021-10-04 13:31:50 +0200

    Implementación de iconos a la sección de "ListadoDeDonadores""
    Se implemento los iconos según la acción. [En Revisión]

--- pages/ListadoDeDonadores.tsx
+++ pages/ListadoDeDonadores.tsx
@@ -1,49 +1,65 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+<link rel="styles" href="globals.css" />;
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
 import { Button, Container, Table } from "react-bootstrap";
-
+import link from "next/link";
 
 const ListadoDeDonadores = (): JSX.Element => {
   return (
     <Container>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Telefono</th>
+            <th>Tipo de Donador</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+            <th>Contactar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>fer</td>
+            <td>llusco</td>
+            <td>Oi@gmail.com</td>
+            <td>362826364</td>
+            <td>Donador de Plasma</td>
+            <td>
+              <Button id="edit" variant="warning">
+                <FontAwesomeIcon style={{}} icon={faEdit} />
+              </Button>
+            </td>
+            <td>
+              <Button id="delete" variant="danger">
+                <FontAwesomeIcon style={{}} icon={faTrashAlt} />
+              </Button>
+            </td>
+            <td>
+              <Button id="llamar" variant="success">
+                <FontAwesomeIcon style={{}} icon={faPhoneAlt} />
+              </Button>
+            </td>
+          </tr>
 
-    <Table striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Email</th>
-          <th>Telefono</th>
-          <th>Tipo de Donador</th>
-          <th>Editar</th>
-          <th>Eliminar</th>
-          <th>Contactar</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>fer</td>
-          <td>llusco</td>
-          <td>Oi@gmail.com</td>
-          <td>362826364</td>
-          <td>Donador de Plasma</td>
-          <Button variant="warning">Warning</Button>{' '}
-          <Button variant="danger">Danger</Button> {' '}
-          <Button variant="success">Success</Button>{' '}
-
-        </tr>
-        <tr>
-          <td>MARIAAAAAA</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>MARCOS</td>
-          <td >Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+          <tr>
+            <td>MARIAAAAAA</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>MARCOS</td>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </Container>
   );
 };

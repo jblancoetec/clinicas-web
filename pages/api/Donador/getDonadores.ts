bad522b3d52fdaace8c12aa6126112f56a888072
Joaquin Blanco <jblanco@etec.uba.ar>
2021-10-18 09:49:01 -0300

    pequeña modificación en la API

--- pages/api/Donador/getDonadores.ts
+++ pages/api/Donador/getDonadores.ts
@@ -1,16 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next" ;
+import { NextApiRequest, NextApiResponse } from "next";
 import conectarDB from "../../../lib/conexionDB";
-import Donador from "../../../models/Donador"; 
+import Donador from "../../../models/Donador";
 import { IDonador } from "../../../models/Donador";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        await conectarDB ();
-        const Donadores: IDonador[] = await Donador.find(req.body);
-        res.status(200).json(Donadores);
-    } catch (error) {
-        console.log(error); 
-        res.status(400);
-    }
-}
-export default handler
+  try {
+    await conectarDB();
+    const Donadores: IDonador[] = await Donador.find();
+    res.status(200).json(Donadores);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+  }
+};
+export default handler;

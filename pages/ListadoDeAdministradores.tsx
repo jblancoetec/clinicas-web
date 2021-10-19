e3611c386002500c84d7333b10372be62c2a2b7a
Joaquin Blanco <89130929+jblancoetec@users.noreply.github.com>
2021-10-19 19:59:08 -0300

    Actualizar el cuestionario
    Agregué preguntas por no.

--- pages/ListadoDeAdministradores.tsx
+++ pages/ListadoDeAdministradores.tsx
@@ -6,39 +6,25 @@ import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Button, Container, Table } from "react-bootstrap";
 import conectarDB from "../lib/conexionDB";
 import { IAdministrador } from "../models/Administrador";
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 interface PropsListadoDeAdministradores {
   administradores: IAdministrador[];
 }
 
-const ListadoDeAdministradores : React.FC<PropsListadoDeAdministradores> = ({administradores}): JSX.Element => {
-  return (
-    <Container>
-      <div>{JSON.stringify(administradores)}</div>
-      <br />
-      <h3 style={{ textAlign: "center" }}>Listado De Administradores</h3>
-      <br />
-
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Email</th>
-            <th>Telefono</th>
-            <th>Usuario</th>
-            <th style={{ textAlign: "center" }}>Editar</th>
-            <th style={{ textAlign: "center" }}>Eliminar</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Fer</td>
-            <td>LLusco</td>
-            <td>@mdo</td>
-            <td>362826364</td>
-            <td>@mdo</td>
+const ListadoDeAdministradores: React.FC<PropsListadoDeAdministradores> = ({
+  administradores,
+}): JSX.Element => {
+  const administradoresARenderizar = administradores.map(
+    (administrador: IAdministrador, index) => {
+      return (
+        <>
+          <tr key={index}>
+            <td>{administrador.nombre}</td>
+            <td>{administrador.apellido}</td>
+            <td>{administrador.email}</td>
+            <td>{administrador.telefono}</td>
+            <td>{administrador.usuario}</td>
 
             <td style={{ textAlign: "center" }}>
               <Button
@@ -56,7 +42,29 @@ const ListadoDeAdministradores : React.FC<PropsListadoDeAdministradores> = ({adm
               </Button>
             </td>
           </tr>
-        </tbody>
+        </>
+      );
+    }
+  );
+  return (
+    <Container>
+      <br />
+      <h3 style={{ textAlign: "center" }}>Listado De Administradores</h3>
+      <br />
+
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Telefono</th>
+            <th>Usuario</th>
+            <th style={{ textAlign: "center" }}>Editar</th>
+            <th style={{ textAlign: "center" }}>Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>{administradoresARenderizar}</tbody>
       </Table>
 
       <Container style={{ textAlign: "center" }}>
@@ -84,14 +92,17 @@ export const getServerSideProps = async () => {
     baseURL: process.env.API_URL,
   };
   try {
-    const res: AxiosResponse = await axios.get("/getAdministradores", config);
+    const res: AxiosResponse = await axios.get(
+      "/Administrador/getAdministradores",
+      config
+    );
     const administradores = res.data;
     return {
       props: { administradores },
     };
   } catch (error) {
     console.log(error);
-		return {
+    return {
       redirect: {
         destination: "/",
         statusCode: 307,

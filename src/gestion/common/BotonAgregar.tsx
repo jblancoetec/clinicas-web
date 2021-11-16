8462f53a40548a29b0393d4c813be667c5f64680
Joaquin <Jblanco@etec.uba.ar>
2021-11-16 16:58:43 -0300

    Update BotonAgregar.tsx

--- src/gestion/common/BotonAgregar.tsx
+++ src/gestion/common/BotonAgregar.tsx
@@ -1,20 +1,19 @@
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Button, Container } from "react-bootstrap";
-import style from "./BotonAgregar.module.css";
-import Formulario from "../Formulario/Editar/Donador";
+import { Button, Modal } from "react-bootstrap";
+import styles from "./BotonAgregar.module.css";
+import { useState } from "react";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   children: React.ReactNode;
 }
 
-const BotonAgregar = ({children }: Props) => {
+const BotonAgregar = ({ children }: Props) => {
   const [FormularioVisible, setFormlarioVisible] = useState(false);
-  
+
   const CerrarFormulario = () => setFormlarioVisible(false);
   const AbrirFormulario = () => setFormlarioVisible(true);
-
 
   return (
     <>
